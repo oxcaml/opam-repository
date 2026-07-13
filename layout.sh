1b6e86cf7f418ee83300c3c6d15f9ba0bb65a57d
David Allsopp <david.allsopp@metastack.com>
2026-07-13 11:21:51 +0100

    Punt angstrom and faraday further

--- layout.sh
+++ layout.sh
@@ -33,7 +33,7 @@ for entry in *; do
               case $entry in
                 angstrom|faraday)
                   # TODO Patches aren't working, so disable them for now
-                  echo "5.2.0minus40:$entry";;
+                  echo "5.4.0-ox1:$entry";;
                 cmarkit|extlib|omd|yojson)
                   echo "5.2.0minus31:$entry";;
                 *)
